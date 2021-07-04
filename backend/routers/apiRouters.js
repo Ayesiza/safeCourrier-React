d2de9dc8a0237b695e7118287af92ea8d6cce2f7
Ayesiza <yesiha2001@gmail.com>
2021-07-04 09:02:11 +0300

    ch(fix all api bugs)

--- backend/routers/apiRouters.js
+++ backend/routers/apiRouters.js
@@ -8,13 +8,14 @@ const router = express.Router();
  //authRoutes
  router.post('/user/signup',authMiddleware.userAreadyExist, UserController.signup);
  router.post('/user/login', UserController.login);
+ router.get('/user', UserController.getUsers);
  //order Routes
  router.post('/order',authMiddleware.getToken,authMiddleware.verifyToken, OrderControllers.createDeliveryOrder);
  router.get('/orders',authMiddleware.getToken,authMiddleware.verifyToken, OrderControllers.getAllOrders);
- router.put('/order/:id',authMiddleware.getToken,authMiddleware.verifyToken, OrderControllers.ChangeDestination);
- router.patch('/orders/:id', OrderControllers.changeLocation);
+ router.put('/order/:id',authMiddleware.getToken,authMiddleware.verifyToken, authMiddleware.userAdmin, OrderControllers.ChangeDestination);
+ router.patch('/orders/:id',authMiddleware.getToken,authMiddleware.verifyToken, OrderControllers.changeLocation);
  router.delete('/order/:id', authMiddleware.getToken,authMiddleware.verifyToken, OrderControllers.cancelDelivery)
-
-
+ 
 
 export default router;
+//MONGODB_URL=mongodb+srv://ayesiza:muslim22@freecluster.nrzxk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority 
