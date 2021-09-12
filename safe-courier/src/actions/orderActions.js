2956075c63b37ffa91f742a4a52a179d796ceba8
Ayesiza <yesiha2001@gmail.com>
2021-09-12 23:12:10 +0300

    fix map is not a function

--- safe-courier/src/actions/orderActions.js
+++ safe-courier/src/actions/orderActions.js
@@ -2,13 +2,19 @@ import axios from "axios";
 import { ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_LIST_FAIL, ORDER_LIST_REQUEST, ORDER_LIST_SUCCESS, ORDER_SAVE_FAIL, ORDER_SAVE_REQUEST, ORDER_SAVE_SUCCESS } from "../constants/orderConstants"
 
 const listOrders =()=> async (dispatch) => {
+    const {token} =JSON.parse(localStorage.getItem("token"));
     try{
         dispatch({type: ORDER_LIST_REQUEST});
-        const { data} = await axios.get('/api/orders');
-        dispatch({type: ORDER_LIST_SUCCESS, payload:data});    
+        const { data} = await axios.get('/api/orders',{
+            headers: {
+                'Authorization':'Bearer ' + token
+            }
+        });
+
+        return dispatch({type: ORDER_LIST_SUCCESS, payload:data});    
     }
     catch(error){
-        dispatch({type: ORDER_LIST_FAIL})
+        // dispatch({type: ORDER_LIST_FAIL})
     }
 };
 
--- safe-courier/src/components/Orders.js
+++ safe-courier/src/components/Orders.js
@@ -1,88 +1,94 @@
-import React, { useEffect}  from 'react';
-import {Link} from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { listOrders } from '../actions/orderActions';
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { listOrders } from "../actions/orderActions";
 
 function Orders(props) {
+  const orderList = useSelector((state) => state.orderList);
+  const { orders, loading, error } = orderList;
+  console.log(orderList)
+  const dispatch = useDispatch();
 
-   const orderList = useSelector(state => state.orderList);
-   const { orders, loading, error} =orderList;
-   const dispatch =useDispatch();
+  useEffect(() => {
+    dispatch(listOrders());
 
+    return () => {
+      //
+    };
+  }, []);
+  return loading ? (
+    <div>Loading...</div>
+  ) : error ? (
+    <div>{error}</div>
+  ) : (
+    <div className="container">
+      <div className="dashboard-content-holder mb-3 ">
+        <h3>USER DASHBOARD</h3>
+      </div>
+      <div className="mt-5 mb-5"></div>
 
-   useEffect(() =>{
-     dispatch(listOrders());
+      <div className="deliver-table ">
+        <h3>Delivery Orders</h3>
+        <table
+          id="datatable"
+          className="table table-striped table-bordered hidden-sm"
+          cellspacing="0"
+          width="100%"
+        >
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>parcelName</th>
+              <th>phone</th>
+              <th>userName</th>
+              <th> receipient</th>
+              <th>destination</th>
+              <th>status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tfoot>
+            <tr>
+              <th>ID</th>
+              <th>parcelName</th>
+              <th>phone</th>
+              <th>userName</th>
+              <th> receipient</th>
+              <th>destination</th>
+              <th>status</th>
+              <th>Action</th>
+            </tr>
+          </tfoot>
+          <tbody>
+            {orders.orders?.map((order) => (
+              <tr key={order._id}>
+                <td>{order._id}</td>
+                <td>
+                  <Link className="link-text" to={"/order/" + order._id}>
+                    {order.parcelName}
+                  </Link>
+                </td>
 
-             
-       return ()=> {
-  //
-       };
-   }, [])
-    return loading ? <div>Loading...</div> :
-    error ? <div>{error}</div> :
-        <div className="container">
-              <div className="dashboard-content-holder mb-3 ">
-                <h3>USER DASHBOARD</h3>           
-              </div>
-              <div className="mt-5 mb-5">
-                
-              </div>
-              
-              <div className="deliver-table ">
-                  <h3>Delivery Orders</h3>  
-                  <table id="datatable" className="table table-striped table-bordered hidden-sm" cellspacing="0" width="100%">    
-                      
-                      <thead >
-                            <tr>
-                            <th>ID</th>                          
-                              <th>parcelName</th>
-                              <th>phone</th>
-                              <th>userName</th>
-                              <th> receipient</th>
-                              <th>destination</th>
-                              <th>status</th>
-                              <th>Action</th>
-                               
-                            </tr>
-                        </thead>
-                        <tfoot>
-                            <tr>  
-                            <th>ID</th>                          
-                              <th>parcelName</th>
-                              <th>phone</th>
-                              <th>userName</th>
-                              <th> receipient</th>
-                              <th>destination</th>
-                              <th>status</th>
-                              <th>Action</th>
-                            </tr>
-                        </tfoot> 
-                        <tbody>
-                           { orders.map( order => (<tr key={order._id}>
-                         
-                            <td>{order._id}</td>
-                            <td><Link className="link-text" to={"/order/" + order._id}>{order.parcelName}</Link></td>  
-                             
-                             <td>{order.phone}</td>
-                             <td>{order.userName}</td>                    
-                             <td>{order.receipient}</td>
-                             <td>{order.destination}</td>
-                             <td>{order.status}</td>
-                             <td>
-                <button type="button"  className="button secondary">Edit</button>
-                {' '}
-                <button type="button"  className="button secondary">Delete</button>
-              </td>
-                         </tr> ))}
-                                            
-                        </tbody>
-                        
-                    </table>
-                    
-                </div>
-                </div>
-    
+                <td>{order.phone}</td>
+                <td>{order.userName}</td>
+                <td>{order.receipient}</td>
+                <td>{order.destination}</td>
+                <td>{order.status}</td>
+                <td>
+                  <button type="button" className="button secondary">
+                    Edit
+                  </button>{" "}
+                  <button type="button" className="button secondary">
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default Orders
-
+export default Orders;
